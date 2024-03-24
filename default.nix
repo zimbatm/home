f836466736b56f856cd4672d60bc404454b99500
zimbatm <zimbatm@users.noreply.github.com>
2024-03-24 12:00:36 +0000

    deploy: 6b991b02aff69957a9623696a211c65ad5d16906

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 { inputs, self, ... }:
 {
-  perSystem = { system, ... }: {
-    packages.docs = inputs.mkdocs-numtide.lib.${system}.mkDocs {
-      name = "zimbatm-docs";
-      src = self;
-    };
+  perSystem =
+    { system, ... }:
+    {
+      packages.docs = inputs.mkdocs-numtide.lib.${system}.mkDocs {
+        name = "zimbatm-docs";
+        src = self;
+      };
 
-    devshells.default.packages = [
-      inputs.mkdocs-numtide.packages.${system}.default
-    ];
-  };
+      devshells.default.packages = [ inputs.mkdocs-numtide.packages.${system}.default ];
+    };
 }
