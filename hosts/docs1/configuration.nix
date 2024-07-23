14266ae0eeb84f3f52f7341ca93371d2f42e0ca3
zimbatm <zimbatm@zimbatm.com>
2024-07-23 09:59:50 +0200

    feat(garnix): publish mkdocs using garnix
    Have fun and see how the system works.

---
+++ hosts/docs1/configuration.nix
@@ -0,0 +1,33 @@
+{
+  inputs,
+  flake,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    # Minimal import because this will be used by Garnix
+    inputs.srvos.nixosModules.mixins-nginx
+  ];
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  # For Garnix
+  boot.loader.grub.device = "/dev/sda";
+  fileSystems."/" = {
+    device = "/dev/sda1";
+    fsType = "ext4";
+  };
+
+  # Use nginx to host the docs
+  services.nginx.virtualHosts.default = {
+    # Not supported by Garnix rn
+    # enableACME = true;
+    # forceSSL = true;
+
+    # Serve the docs
+    locations."/".root = flake.packages.${pkgs.system}.docs;
+  };
+
+  system.stateVersion = "24.05";
+}
