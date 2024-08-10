66628b179fa240b899e6a39bd584ab3fe519e2cc
zimbatm <zimbatm@zimbatm.com>
2024-08-10 12:15:17 +0200

    chore(flake): add renovatebot workaround
    See https://github.com/renovatebot/renovate/issues/29721

--- flake.nix
+++ flake.nix
@@ -14,6 +14,8 @@
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
     nix-index-database.url = "github:Mic92/nix-index-database";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
+    # Trick renovate into working: github:NixOS/nixpkgs/nixpkgs-unstable
+    # see https://github.com/renovatebot/renovate/issues/29721
     nixpkgs.follows = "srvos/nixpkgs"; # use the version of nixpkgs that has been tested with SrvOS
     nixvim = {
       url = "github:nix-community/nixvim";
