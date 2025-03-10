85cd5b5ec76f89ba076b33c805f74caeb8c20d7f
zimbatm <zimbatm@zimbatm.com>
2025-03-10 09:44:15 +0100

    no1: fix eval

--- hosts/no1/configuration.nix
+++ hosts/no1/configuration.nix
@@ -41,7 +41,7 @@
   hardware.nvidia-container-toolkit.enable = true;
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.nvidiaSettings = true;
-  hardware.nvidia.open = false;
+  hardware.nvidia.open = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
   services.xserver.videoDrivers = [ "nvidia" ];
 
