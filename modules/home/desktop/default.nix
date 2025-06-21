b0b48ab0a853df343265b70dc92b09410c3aa6b4
zimbatm <zimbatm@zimbatm.com>
2025-06-21 22:21:15 +0200

    install ghostty

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -5,6 +5,15 @@
   programs.firefox.enable = true;
   programs.vscode.enable = true;
 
+  programs.ghostty = {
+    enable = true;
+
+    settings = {
+      # theme = "catppuccin-mocha";
+      font-size = 16;
+    };
+  };
+
   home.packages = with pkgs; [
     # Graphical
     brave
