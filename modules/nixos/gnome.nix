117e488b6a5fe4a2060438544ec0e67dc2608069
zimbatm <zimbatm@zimbatm.com>
2025-07-02 18:15:57 +0200

    use gcr-ssh-agent

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -1,10 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
   # Disable indexing service to save power
   services.gnome.localsearch.enable = false;
+
+  services.gnome.gcr-ssh-agent.enable = true;
+
+  programs.ssh.askPassword = "${pkgs.seahorse}/libexec/seahorse/ssh-askpass";
+  programs.ssh.enableAskPassword = true;
 
   services.libinput.enable = true;
 }
