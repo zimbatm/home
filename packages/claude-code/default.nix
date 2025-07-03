99cfdf025eec7b91e1fc88c2b7a8865580822dad
zimbatm <zimbatm@zimbatm.com>
2025-07-03 17:49:12 +0200

    claude-code: fix websocket error

--- packages/claude-code/default.nix
+++ packages/claude-code/default.nix
@@ -8,6 +8,7 @@ in
   cp -r --no-preserve=mode ${cl} $out
   chmod +x $out/bin/claude
   wrapProgram $out/bin/claude \
+    --unset DEV \
     --set DISABLE_TELEMETRY 1 \
     --set CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC 1 \
     --set DISABLE_NON_ESSENTIAL_MODEL_CALLS 1 \
