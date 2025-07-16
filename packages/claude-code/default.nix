6cc1376d85220432c7523569133271fbc6c3bb7a
zimbatm <zimbatm@zimbatm.com>
2025-07-16 10:04:06 +0200

    claude-code: 1.0.51 -> 1.0.53

--- packages/claude-code/default.nix
+++ packages/claude-code/default.nix
@@ -1,18 +1,1 @@
-{ pkgs }:
-let
-  cl = pkgs.claude-code;
-in
-pkgs.runCommand cl.name
-  {
-    buildInputs = [ pkgs.makeWrapper ];
-  }
-  ''
-    cp -r --no-preserve=mode ${cl} $out
-    chmod +x $out/bin/claude
-    wrapProgram $out/bin/claude \
-      --unset DEV \
-      --set DISABLE_TELEMETRY 1 \
-      --set CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC 1 \
-      --set DISABLE_NON_ESSENTIAL_MODEL_CALLS 1 \
-      --set ANTHROPIC_SMALL_FAST_MODEL claude-haiku-3
-  ''
+{ pkgs, ... }: pkgs.callPackage ./package.nix { }
