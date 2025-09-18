4aafafc2bb452ff8c934ff60bfdd0c128aeb3f69
zimbatm <zimbatm@zimbatm.com>
2025-09-18 08:11:24 +0200

    web1: remove

--- hosts/web1/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-exec nixos-anywhere -f .#web1 root@95.216.188.155
--- hosts/web1/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-if [[ $# == 0 ]]; then
-  set -- switch
-fi
-
-exec nixos-rebuild --flake .#web1 --target-host root@95.216.188.155 "$@"
