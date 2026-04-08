26dbaa0cc79782e375c120bbb730979b1cd103a3
Jonas Pfenniger Chevalier <jonasc@anthropic.com>
2026-04-08 11:51:11 +0000

    relay -> relay1 (fresh Hetzner box, old relay decommissioned separately)

--- kin.nix
+++ kin.nix
@@ -19,7 +19,7 @@ in
     nv1 = { host = "nv1.zt"; tags = [ "desktop" ]; profile = "none"; };
     p1 = { host = "p1.local"; tags = [ "laptop" ]; profile = "none"; };
     web2 = { host = "89.167.46.118"; tags = [ "server" ]; profile = "hetzner-cloud"; };
-    relay1 = { host = "PENDING"; tags = [ "server" "relay" ]; profile = "hetzner-cloud"; };
+    relay1 = { host = "95.216.188.155"; tags = [ "server" "relay" ]; profile = "hetzner-cloud"; };
   };
 
   services.identity = { domain = "ztm"; hosts = [ "all" ]; };
